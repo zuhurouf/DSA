a88bb2ec0115a2f85d692d4ff4d35a6116fec0ce
zuhurouf <zuhuroufsinu712000@gmail.com>
2024-06-09 18:57:01 +0530

    Added deletion operation in linked list

--- DS/Linked-List.cpp
+++ DS/Linked-List.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 
 using namespace std;
@@ -15,11 +16,36 @@ struct node* HEAD = NULL;
 
 void display() {
     struct node* CURR_NODE = HEAD;
+    int pos = 0;
     cout << "\n";
     while (CURR_NODE != NULL) {
-        cout << CURR_NODE->data << " ";
+        cout << CURR_NODE->data << " -> ";
         CURR_NODE = CURR_NODE->ptr;
+        pos++;
     }
+}
+
+
+void delete_at_pos(int data) {
+    struct node* CURR_NODE, *PREV_NODE;
+    if (HEAD == NULL) {
+        cout << "Linked list is empty" << endl;
+    }
+    else {
+        if (HEAD->data == data) {
+            HEAD = HEAD->ptr;
+        }
+        else {
+            CURR_NODE = HEAD;
+            while (CURR_NODE != NULL and CURR_NODE->data != data) {
+                PREV_NODE = CURR_NODE;
+                CURR_NODE = CURR_NODE->ptr;
+            }
+            PREV_NODE->ptr = CURR_NODE->ptr;
+        }
+    }
+
+    display();
 }
 
 
@@ -28,19 +54,13 @@ void insert(int data, int pos) {
     struct node* CURR_NODE;
     struct node* n = (struct node*)malloc(sizeof(struct node));
     n->data = data;
-    if (HEAD == NULL) {
-        n->ptr = NULL;
-        HEAD = n;
+    CURR_NODE = HEAD;
+    while (curr_pos != pos) {
+        CURR_NODE = CURR_NODE->ptr;
+        curr_pos++;
     }
-    else {
-        CURR_NODE = HEAD;
-        while (curr_pos != pos) {
-            CURR_NODE = CURR_NODE->ptr;
-            curr_pos++;
-        }
-        n->ptr = CURR_NODE->ptr;
-        CURR_NODE->ptr = n;
-    }
+    n->ptr = CURR_NODE->ptr;
+    CURR_NODE->ptr = n;
 
     display();
 }
@@ -85,7 +105,7 @@ void insert_at_front(int data) {
 int main() {
     int choice, data, pos;
     while (true) {
-        cout << "\n1 Insert" << endl;
+        cout << "\n1 Insert\n2 Delete" << endl;
         cin >> choice;
         switch(choice) {
             case 1:
@@ -104,6 +124,11 @@ int main() {
                     cin >> pos;
                     insert(data, pos);
                 }
+                break;
+            case 2:
+                cout << "Delete?: ";
+                cin >> data;
+                delete_at_pos(data);
                 break;
         }
     }
