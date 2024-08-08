10a5e7abcd1ece1055d7dac66dc34e1079c30702
Zuhurouf <65843459+zuhurouf@users.noreply.github.com>
2024-08-08 20:11:34 +0530

    Updated code to add an edge and vertex and to remove an edge

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -4,8 +4,19 @@
 using namespace std;
 
 
+void display(int n, vector<vector<int>>& edges) {
+    cout << "Displaying adjecency matrix" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << edges[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+
 int main() {
-    int n;
+    int n, opt, n1, n2;
     char connect;
     cout << "Number of vertices: ";
     cin >> n;
@@ -22,12 +33,70 @@ int main() {
             }
         }
     }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << edges[i][j] << " ";
+    display(n, edges);
+    
+    
+    while (true) {
+        cout << "1. Add a vertex\n2. Add an edge\n3. Remove an edge" << endl;
+        cin >> opt;
+        switch (opt) {
+            case 1:
+                n += 1;
+                edges.resize(n);
+                for (auto& row: edges) {
+                    row.resize(n, 0);
+                }
+                // edges[n][i]
+                for(int i = 0; i < n; i++) {
+                    if (i != n-1) {
+                        cout << "Connect " << n << " and " << i+1 << "(y/n)?";
+                        cin >> connect;
+                        if (connect == 'y') {
+                            edges[n-1][i] = 1;
+                        }
+                    }
+                }
+                // edges[i][n]
+                for(int i = 0; i < n; i++) {
+                    if (i != n-1) {
+                        cout << "Connect " << i+1 << " and " << n << "(y/n)?";
+                        cin >> connect;
+                        if (connect == 'y') {
+                            edges[i][n-1] = 1;
+                        }
+                    }
+                }
+                display(n, edges);
+                break;
+            
+            case 2:
+                cout << "Create an edge between: ";
+                cin >> n1 >> n2;
+                if (edges[n1 - 1][n2 - 1] != 1) {
+                    edges[n1 - 1][n2 - 1] = 1;
+                    display(n, edges);
+                }
+                else {
+                    cout << "There is an edge between " << n1 << " and " << n2 << endl;
+                }
+                break;
+            
+            case 3:
+                cout << "Remove an edge between: ";
+                cin >> n1 >> n2;
+                if (edges[n1 - 1][n2 - 1] == 1) {
+                    edges[n1 - 1][n2 - 1] = 0;
+                    display(n, edges);
+                }
+                else {
+                    cout << "There  is no edge between " << n1 << " and " << n2 << endl;
+                }
+                break;
+            
+            default:
+                display(n, edges);
+                break;
         }
-        cout << "\n";
     }
     return 0;
 }
